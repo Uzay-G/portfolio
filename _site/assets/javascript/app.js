7a5ba7e9d16579d77e3dade1fd005d08191d660d
Debian <admin@ec2-35-180-103-103.eu-west-3.compute.amazonaws.com>
2020-04-29 07:53:35 +0000

    meaningless

--- _site/assets/javascript/app.js
+++
@@ -1,5 +0,0 @@
-AOS.init();
-document.querySelector('.Burger').addEventListener('click', function () {
-  document.querySelector('.nav').classList.toggle('is-open');
-  document.querySelector(".nav").classList.toggle('overlay-open');
-});
