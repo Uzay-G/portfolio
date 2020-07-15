4e54fd84d674d7bc0d243bc1fdec911b97b98f01
Uzay-G <uzgirit@gmail.com>
2020-07-16 00:33:28 +0200

    simply homepage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,1 @@
 AOS.init();
-document.querySelector('.Burger').addEventListener('click', function () {
-  document.querySelector('.nav').classList.toggle('is-open');
-  document.querySelector(".nav").classList.toggle('overlay-open');
-});
